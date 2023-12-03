0ae573f1d088b0d3060f770a10861b0dc02b0c33
Brian Libetario <iglibetario@gmail.com>
2023-12-03 16:52:01 +0800

    Components

--- personal-website/src/components/FooterComp.vue
+++ personal-website/src/components/FooterComp.vue
@@ -1,7 +1,7 @@
 <template>
     <article class="message">
   <div class="message-body">
-  <b>&copy; 2019-2023 Braveium Studios</b> - <a href="https://twitter.com/iglibetario">Twitter</a> | <a href="#">YouTube</a>
+  <b>&copy; 2019-2023 Braveium Studios</b>
   </div>
   </article>
   </template>
